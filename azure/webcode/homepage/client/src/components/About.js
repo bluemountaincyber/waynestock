8d808373831ebd81ce023222bd72b58e00524b94
ryananicholson <ryananicholson@gmail.com>
2024-08-13 11:46:17 -0400

    fix: homepage update

--- azure/webcode/homepage/client/src/components/About.js
+++ azure/webcode/homepage/client/src/components/About.js
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <div>
             <h1>About</h1>
-            <p>One night, Wayne has a dream. Jim Morrison appears, along with a “weird naked Indian” (because, why not?). They tell Wayne that his destiny is to organize a major music festival. Wayne wakes up, inspired, and decides to create the ultimate rock event: <b>WayneStock</b>!</p>
+            <p>One night, Wayne has a dream. Jim Morrison appears, along with another “strange character”. They tell Wayne that his destiny is to organize a major music festival. Wayne wakes up, inspired, and decides to create the ultimate rock event: <b>WayneStock</b>!</p>
         </div>
     );
 }
