d44ef167b66b9ac454c67845fa564927a268a6b6
ryananicholson <ryananicholson@gmail.com>
2024-08-13 11:25:09 -0400

    feat: Add Defender

--- azure/security.tf
+++ azure/security.tf
@@ -1,15 +1,15 @@
-# resource "azurerm_subscription_policy_assignment" "mcsb-assignment" {
-#   name                 = "mcsb"
-#   display_name         = "Microsoft Cloud Security Benchmark"
-#   policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/1f3afdf9-d0c9-4c3d-847f-89da613e70a8"
-#   subscription_id      = data.azurerm_subscription.current.id
-# }
+resource "azurerm_subscription_policy_assignment" "mcsb-assignment" {
+  name                 = "mcsb"
+  display_name         = "Microsoft Cloud Security Benchmark"
+  policy_definition_id = "/providers/Microsoft.Authorization/policySetDefinitions/1f3afdf9-d0c9-4c3d-847f-89da613e70a8"
+  subscription_id      = data.azurerm_subscription.current.id
+}
 
-# resource "azurerm_security_center_subscription_pricing" "mdc-servers" {
-#   tier          = "Standard"
-#   resource_type = "VirtualMachines"
-#   subplan       = "P2"
-# }
+resource "azurerm_security_center_subscription_pricing" "mdc-servers" {
+  tier          = "Standard"
+  resource_type = "VirtualMachines"
+  subplan       = "P2"
+}
 
 resource "azurerm_log_analytics_workspace" "la-workspace" { 
   name = "wslaw" 
@@ -18,10 +18,10 @@ resource "azurerm_log_analytics_workspace" "la-workspace" {
   sku = "PerGB2018" 
 }
 
-# resource "azurerm_security_center_workspace" "la-workspace" {
-#   scope        = data.azurerm_subscription.current.id
-#   workspace_id = azurerm_log_analytics_workspace.la-workspace.id
-# }
+resource "azurerm_security_center_workspace" "la-workspace" {
+  scope        = data.azurerm_subscription.current.id
+  workspace_id = azurerm_log_analytics_workspace.la-workspace.id
+}
 
 resource "azurerm_sentinel_log_analytics_workspace_onboarding" "la-onboarding" {
   workspace_id                 = azurerm_log_analytics_workspace.la-workspace.id
