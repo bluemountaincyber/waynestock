b90d95dd31d1111c73e4ab5a2d4e9eb76c4ee90d
ryananicholson <ryananicholson@gmail.com>
2024-08-13 12:20:45 -0400

    fix: Make response even bigger

--- aws/webcode/volunteer/server/times.php
+++ aws/webcode/volunteer/server/times.php
@@ -2,7 +2,7 @@
 $shift = $_GET["shift"];
 
 $times_file = fopen($shift, "r") or die("Unable to access hours!");
-$times = fread($times_file, 1200);
+$times = fread($times_file, 2000);
 fclose($times_file);
 
 echo $times;
