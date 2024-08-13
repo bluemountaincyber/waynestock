16aa15d4769d503b3c87b0ec0d136add915c22b5
ryananicholson <ryananicholson@gmail.com>
2024-08-13 12:07:20 -0400

    fix: Allow larger files

--- aws/webcode/volunteer/server/times.php
+++ aws/webcode/volunteer/server/times.php
@@ -2,7 +2,7 @@
 $shift = $_GET["shift"];
 
 $times_file = fopen($shift, "r") or die("Unable to access hours!");
-$times = fread($times_file, 1000);
+$times = fread($times_file, 1200);
 fclose($times_file);
 
 echo $times;
