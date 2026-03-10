61bbba1f3c178ac39f215c4b464be0e58ecaaeeb
ryananicholson <ryananicholson@gmail.com>
2026-03-10 14:15:57 -0400

    fix: securityhub standard timeouts

--- aws/security.tf
+++ aws/security.tf
@@ -36,6 +36,11 @@ resource "aws_securityhub_account" "security_hub" {
 resource "aws_securityhub_standards_subscription" "aws_best_practices" {
   depends_on    = [aws_securityhub_account.security_hub]
   standards_arn = "arn:aws:securityhub:${var.region}::standards/aws-foundational-security-best-practices/v/1.0.0"
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
 }
 
 resource "aws_config_configuration_recorder" "cr" {
